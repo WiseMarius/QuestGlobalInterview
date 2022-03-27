61f2cdd680973dd7270099551f55a41c62c77e68
Marius Demeter <marius.demeter96@yahoo.com>
2022-03-27 14:20:29 +0300

    Reading issue fix

--- Converter/Converter/Info.cs
+++ Converter/Converter/Info.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace Converter
 {
     internal class Info
     {
-        public IList<string> Directors { get; set; }
+        [JsonProperty(PropertyName = "directors")]
+        public List<string> Directors { get; set; } = new List<string>();
+
+        [JsonProperty(PropertyName = "release_date")]
         public DateTime ReleaseDate { get; set; }
+
+        [JsonProperty(PropertyName = "rating")]
         public float Rating { get; set; }
-        public IList<string> Genres { get; set; }
+
+        [JsonProperty(PropertyName = "genres")]
+        public List<string> Genres { get; set; } = new List<string>();
+
+        [JsonProperty(PropertyName = "image_url")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty(PropertyName = "plot")]
+        public string Plot { get; set; }
+
+        [JsonProperty(PropertyName = "rank")]
+        public uint Rank { get; set; }
+
+        [JsonProperty(PropertyName = "running_time_secs")]
+        public uint RunningTimeSecs { get; set; }
+
+        [JsonProperty(PropertyName = "actors")]
+        public List<string> Actors { get; set; } = new List<string>();
     }
 }
--- Converter/Converter/Movie.cs
+++ Converter/Converter/Movie.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Converter
 {
     internal class Movie
     {
+        [JsonProperty(PropertyName = "year")]
         public uint Year { get; set; }
-        public string Title { get; set; }
-        public Info Info { get; set; }
-        public string ImageUrl { get; set; }
-        public string Plot { get; set; }
-        public uint Rank { get; set;}
-        public uint RunningTimeSecs{ get; set; }
-        public IList<string> Actors { get; set; }
 
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "info")]
+        public Info Info { get; set; }
     }
 }
