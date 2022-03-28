8be6ea2ca3b544751f78c27f94ab3ec384ccedfb
Marius Demeter <marius.demeter96@yahoo.com>
2022-03-28 18:18:35 +0300

    Added GetMoviesFromYearByName

--- WebApplication/WebApplication/Controllers/MoviesController.cs
+++ WebApplication/WebApplication/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using WebApplication.Models;
 
@@ -16,6 +17,27 @@ namespace WebApplication.Controllers
                 var movies = context.Movies.SqlQuery("SELECT * FROM Movie WHERE Year == @Year ORDER BY ReleaseDate LIMIT 4", new System.Data.SQLite.SQLiteParameter("@Year", year));
 
                 foreach(var movie in movies)
+                {
+                    results.Add(movie);
+                }
+            }
+
+            return results;
+        }
+
+        // GET api/Movies?year=2010&name=Ins
+        public IEnumerable<Movie> GetMoviesFromYearByName(int year, string name)
+        {
+            var results = new List<Movie>();
+
+            using (var context = new Database.Context())
+            {
+                var movies = from movie in context.Movies
+                              where movie.Title.Contains(name)
+                              where movie.Year == year 
+                              select movie;
+
+                foreach (var movie in movies)
                 {
                     results.Add(movie);
                 }
